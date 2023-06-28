89a37d52d16ba1d80bb3648a50a3c19f03c2847a
Adebola Adesina <dr3apap@gmail.com>
2023-06-28 15:07:57 -0700

    WIP:Add a cli interface/lib

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+/**
+ * Entry point for the API 
+ */
+
+
 // Dependencies
 const server = require('./lib/server');
 const workers = require('./lib/workers');
+const cli = require('./lib/cli');
 
-app = {};
+// App name space 
+const app = {};
 
+// App Init function
 app.init = function(){
+// Server Start/Init
 server.init();
+// Workers Start/Init
 workers.init();
+
+// Start the CLI, but starts it last so that the console doesn't get clogged by outputs we want to be able to
+// see the output of the servers starting and the workers too.
+setTimeout(function(){
+    cli.init();
+},50);
 };
 
+// Call/Execute
 app.init();
+
+// Export the app
 module.exports = app;
